ef16973a3a9c928523b14683f78ba120a7b8ec42
Nonaka <c0b221183f@edu.teu.ac.jp>
2023-06-20 14:11:13 +0900

    練習1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,3 +1,4 @@
+import random
 import sys
 import pygame as pg
 
@@ -11,6 +12,13 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    bakudan = pg.Surface((20, 20))
+    pg.draw.circle(bakudan, (255, 0, 0), (10, 10), 10)
+    bakudan.set_colorkey((0, 0, 0))
+    bakudan_x = random.randint(0, WIDTH)
+    bakudan_y = random.randint(0, HEIGHT)
+    bakudan_rct = bakudan.get_rect()
+    bakudan_rct.center = bakudan_x, bakudan_y
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -20,6 +28,7 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(bakudan, [bakudan_x, bakudan_y])
         pg.display.update()
         tmr += 1
         clock.tick(10)
