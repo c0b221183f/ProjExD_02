e7ea65c9c837867d318ee416bd308b7214c60345
Nonaka <c0b221183f@edu.teu.ac.jp>
2023-06-20 15:22:16 +0900

    練習5

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -11,19 +11,19 @@ delta = {
     pg.K_RIGHT: (+5, 0),
 }
 
-def is_inner_display(rct: pg.Rect) -> tuple[bool, bool]:
+
+def check_bound(rect: pg.Rect) -> tuple[bool, bool]:
     """
-    こうかとんと爆弾が画面の外にいるか判定する関数
-    引数: kk_rct or bd_rct
-    戻り値: 横方向. 縦方向の判定結果タプル (True: 画面内, False: 画面外)
+    こうかとんRect, 爆弾Rectが画面外 or 画面内かを判定する関数
+    引数: こうかとんRect or 爆弾Rect
+    戻り値: 横方向, 縦方向の判定結果タプル（True: 画面内, False: 画面外）
     """
-    x, y = True, True
-    if rct.left < 0 or WIDTH < rct.right:  # 横方向の判定
-        x = False
-    if rct.top < 0 or HEIGHT < rct.bottom:  # 縦方向の判定
-        y = False
-    return x, y
-        
+    yoko, tate = True, True
+    if rect.left < 0 or WIDTH < rect.right:  # 横方向判定
+        yoko = False
+    if rect.top < 0 or HEIGHT < rect.bottom:  # 縦方向判定
+        tate = False
+    return yoko, tate
 
 
 def main():
@@ -32,14 +32,17 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    # こうかとんSurface（kk_img）からこうかとんRect（kk_rct）を抽出する
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
     bd_img = pg.Surface((20, 20))
-    bd_img.set_colorkey((0, 0, 0))
+    bd_img.set_colorkey((0, 0, 0))  # 黒い部分を透明にする
     pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
     x = random.randint(0, WIDTH)
     y = random.randint(0, HEIGHT)
+    # 爆弾Surface（bd_img）から爆弾Rect（bd_rct）を抽出する
     bd_rct = bd_img.get_rect()
+    # 爆弾Rectの中心座標を乱数で指定する
     bd_rct.center = x, y 
     vx, vy = +5, +5
 
@@ -49,26 +52,29 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-            
+        
+        if kk_rct.colliderect(bd_rct):
+            print("Game Over")
+            return  # Game Over
+
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         for k, mv in delta.items():
-            if key_lst[k]:
+            if key_lst[k]: 
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
         kk_rct.move_ip(sum_mv)
-        if is_inner_display(kk_rct) != (True, True):
+        if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
-
+ 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, kk_rct)
         bd_rct.move_ip(vx, vy)
-        x, y = is_inner_display(bd_rct)
-        if not x:  # 横方向に画面外に出ていたら
+        yoko, tate = check_bound(bd_rct)
+        if not yoko:
             vx *= -1
-        if not y:  # 縦方向に画面外に出ていたら
+        if not tate:
             vy *= -1
-        
         screen.blit(bd_img, bd_rct)
         pg.display.update()
         tmr += 1
