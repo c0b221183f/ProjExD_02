3c855db60a43071a770cb40201bcafe669daa405
Nonaka <c0b221183f@edu.teu.ac.jp>
2023-06-20 15:18:30 +0900

    練習4

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -11,6 +11,20 @@ delta = {
     pg.K_RIGHT: (+5, 0),
 }
 
+def is_inner_display(rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    こうかとんと爆弾が画面の外にいるか判定する関数
+    引数: kk_rct or bd_rct
+    戻り値: 横方向. 縦方向の判定結果タプル (True: 画面内, False: 画面外)
+    """
+    x, y = True, True
+    if rct.left < 0 or WIDTH < rct.right:  # 横方向の判定
+        x = False
+    if rct.top < 0 or HEIGHT < rct.bottom:  # 縦方向の判定
+        y = False
+    return x, y
+        
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -43,11 +57,18 @@ def main():
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
         kk_rct.move_ip(sum_mv)
-        
+        if is_inner_display(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, kk_rct)
         bd_rct.move_ip(vx, vy)
+        x, y = is_inner_display(bd_rct)
+        if not x:  # 横方向に画面外に出ていたら
+            vx *= -1
+        if not y:  # 縦方向に画面外に出ていたら
+            vy *= -1
+        
         screen.blit(bd_img, bd_rct)
         pg.display.update()
         tmr += 1
